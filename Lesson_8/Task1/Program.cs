e9a64bb9c5c76924946edbe039cd9b8d5989d10d
stanlocktop <wlad-test@ukr.net>
2023-09-24 22:43:37 +0300

    finish lesson_8

--- Lesson_8/Task1/Program.cs
+++ Lesson_8/Task1/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-
 class Address
 {
     private string index;
@@ -14,41 +13,35 @@ class Address
         get { return index; }
         set { index = value; }
     }
-
     public string Country
     {
         get { return country; }
         set { country = value; }
     }
-
     public string City
     {
         get { return city; }
         set { city = value; }
     }
-
     public string Street
     {
         get { return street; }
         set { street = value; }
     }
-
     public string House
     {
         get { return house; }
         set { house = value; }
     }
-
     public string Apartment
     {
         get { return apartment; }
         set { apartment = value; }
     }
-
     public void DisplayAddress()
     {
         Console.OutputEncoding = System.Text.Encoding.Unicode;
-        Console.ForegroundColor = ConsoleColor.Magenta; // устанавливаем цвет
+        Console.ForegroundColor = ConsoleColor.Blue; // устанавливаем цвет
         Console.WriteLine($"Індекс: {Index}");
         Console.WriteLine($"Країна: {Country}");
         Console.WriteLine($"Місто: {City}");
@@ -62,15 +55,12 @@ class Program
     static void Main(string[] args)
     {
         Address myAddress = new Address();
-
-        // Заповнюємо поля адреси
-        myAddress.Index = "12345";
+        myAddress.Index = "49000";
         myAddress.Country = "Україна";
-        myAddress.City = "Київ";
-        myAddress.Street = "Вулиця Шевченка";
-        myAddress.House = "25";
+        myAddress.City = "Дніпро";
+        myAddress.Street = "Проспект Гагаріна";
+        myAddress.House = "15";
         myAddress.Apartment = "12";
-
         Console.ForegroundColor = ConsoleColor.Green; // устанавливаем цвет
         Console.WriteLine("Поштова адреса:");
         myAddress.DisplayAddress();
