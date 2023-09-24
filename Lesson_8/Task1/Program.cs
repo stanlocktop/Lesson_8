8e2f394a21d81862b08b68853ba56b9986ce5e46
stanlocktop <wlad-test@ukr.net>
2023-09-24 21:39:06 +0300

    create new task

---
+++ Lesson_8/Task1/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Task1
+{
+//    1) Написати програму, яка перевіряє введене число на парність/непарність.
+
+//13 -> "непарне"
+
+//22 -> "парне"
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            Console.WriteLine("Введіть число: ");
+            int a = Convert.ToInt32(Console.Read());
+            {
+                if (a % 2 == 0) Console.WriteLine("Парне");
+                else Console.WriteLine("Непарне");
+            }
+            Console.ReadKey();
+        }
+    }
+}
