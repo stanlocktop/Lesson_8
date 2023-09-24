cb2bea44a904f412d396d2d3c46f428348de29bd
stanlocktop <wlad-test@ukr.net>
2023-09-24 23:07:19 +0300

    finish home work

---
+++ Lesson_8/Task1/Address.cs
@@ -0,0 +1,59 @@
+using System;
+
+class Address
+{
+	private string index; // індекс
+	private string country; // країна
+	private string city; // місто 
+	private string street; // вулиця
+	private string house; // номер будинку
+	private string apartment; // номер квартири
+
+	public string Index
+	{
+		get { return index; }
+		set { index = value; }
+	}
+
+	public string Country
+	{
+		get { return country; }
+		set { country = value; }
+	}
+
+	public string City
+	{
+		get { return city; }
+		set { city = value; }
+	}
+
+	public string Street
+	{
+		get { return street; }
+		set { street = value; }
+	}
+
+	public string House
+	{
+		get { return house; }
+		set { house = value; }
+	}
+
+	public string Apartment
+	{
+		get { return apartment; }
+		set { apartment = value; }
+	}
+
+	public void DisplayAddress()
+	{
+		Console.OutputEncoding = System.Text.Encoding.Unicode;
+		Console.ForegroundColor = ConsoleColor.Blue; // устанавливаем цвет
+		Console.WriteLine($"Індекс: {Index}");
+		Console.WriteLine($"Країна: {Country}");
+		Console.WriteLine($"Місто: {City}");
+		Console.WriteLine($"Вулиця: {Street}");
+		Console.WriteLine($"Номер будинку: {House}");
+		Console.WriteLine($"Номер квартири: {Apartment}");
+	}
+}
